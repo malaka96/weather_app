496fa747abb8427dc24bf9ff896f6a67a2c55da0
malaka96 <madhubhashana655@gmail.com>
2025-04-10 10:45:52 +0530

    card modify

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WeatherScreen extends StatelessWidget {
@@ -21,23 +23,44 @@ class WeatherScreen extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               child: Card(
-                child: Column(
-                  children: [
-                    Text(
-                      '300°F',
-                      style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
+                elevation: 6,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            '300°F',
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          Icon(Icons.cloud, size: 62),
+                          const SizedBox(height: 16),
+                          Text(
+                            'Rain',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    Icon(Icons.cloud, size: 62),
-                    Text('Rain', style: TextStyle(fontSize: 20)),
-                  ],
+                  ),
                 ),
               ),
             ),
             const SizedBox(height: 20),
-            const Placeholder(fallbackHeight: 150),
+            const Card(child: Column(children: [Text('Hello')])),
             const SizedBox(height: 20),
             const Placeholder(fallbackHeight: 250),
           ],
